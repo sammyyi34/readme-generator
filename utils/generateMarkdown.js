f0c6891da7f8113d8aea0d6758401f10d37740a0
Sammy <sammyhyi@gmail.com>
2023-04-29 14:43:23 -0400

    made renderLicenseSection function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,11 +2,11 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license === 'MIT License') {
-    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]';
+    return 'https://img.shields.io/badge/License-MIT-yellow.svg';
   } else if (license === 'Apache License 2.0') {
-    return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]';
+    return 'https://img.shields.io/badge/License-Apache_2.0-blue.svg';
   } else if (license === 'GNU General Public License v3.0') {
-    return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]';
+    return 'https://img.shields.io/badge/License-GPLv3-blue.svg';
   } else {
     return '';
   }
@@ -28,12 +28,18 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license) {
+    return `This project is licensed under the [${license}](${renderLicenseBadge(license)}) license. ${renderLicenseLink(license)}`;
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+https://github.com/
 `;
 }
 
