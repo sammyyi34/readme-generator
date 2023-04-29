7bf28aaa293ee0e30fab3b5cf1088925f9990347
Sammy <sammyhyi@gmail.com>
2023-04-29 14:24:36 -0400

    made renderLicenseLink function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,11 +2,11 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license === 'MIT License') {
-    return 'This project is licensed under the MIT License.';
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]';
   } else if (license === 'Apache License 2.0') {
-    return 'This project is licensed under the Apache License 2.0.';
+    return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]';
   } else if (license === 'GNU General Public License v3.0') {
-    return 'This project is licensed under the GNU General Public License v3.0.';
+    return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]';
   } else {
     return '';
   }
@@ -14,7 +14,17 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'MIT License') {
+    return 'https://opensource.org/licenses/MIT';
+  } else if (license === 'Apache License 2.0') {
+    return 'https://opensource.org/licenses/Apache-2.0';
+  } else if (license === 'GNU General Public License v3.0') {
+    return 'https://opensource.org/licenses/GPL-3.0';
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
